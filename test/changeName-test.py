722ff72c965c90c9421646ce02091a6977ef3563
unknown <ehendrich05111@gmail.com>
2022-10-19 15:14:11 -0400

    remove pycache and update tests and functionality for changeName endpoint

--- test/changeName-test.py
+++ test/changeName-test.py
@@ -1,77 +1,66 @@
 import requests
 import json
 import unittest
-import bson
 import random
 import string
 import pymongo
 from decouple import config
+import bson
+import jwt
 
 class Test_Backend(unittest.TestCase):
+    TEST_USER_EMAIL = "TEST_EMAIL"
+
 
     @classmethod
     def setUpClass(self) -> None:
-        self.base_url = "http://localhost:9000/changeName"
+        self.base_url = "http://localhost:9000/changeName/"
         connectionString = config("CONNECTION_STRING")
         client = pymongo.MongoClient(connectionString)
         db = client['test']
         self.collection = db['users']
+
+        self.session = requests.Session()
+        self.session.post("http://localhost:9000/signUp", {"email": self.TEST_USER_EMAIL, "password": "qwertyuiop1!", "firstName": "firstName", "lastName": "lastName"})
+        resp = self.session.post("http://localhost:9000/signIn", {"email": self.TEST_USER_EMAIL, "password": "qwertyuiop1!"})
+        token = resp.json()['data']['token']
+
+        self.session.headers.update({'Authorization': 'JWT ' + token})
+
+    @classmethod
+    def tearDownClass(self) -> None:
+        self.collection.delete_one({"email": self.TEST_USER_EMAIL})
+        self.session.close()
 
     def testEmptyBody(self):
         response = requests.post(self.base_url, {})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json()["message"], "Error with request information")
 
-    def testNonExistentId(self):
-        userID = bson.ObjectId("111111111111111111111111")
-        response = requests.post(self.base_url, {"userID": userID, "newFirstName": "test name"})
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json()["message"], "No user with that ID")
 
     def testChangeFirstName(self):
-        try:
-            userID = bson.ObjectId(''.join(random.choices(string.digits, k = 24)))
-            self.collection.insert_one({"_id": userID, "firstName": "first test name", "lastName": "last test name"})
-            response = requests.post(self.base_url, {"userID": userID, "newFirstName": "test name"})
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json()["data"]["newFirstName"], "test name")
-            test_user = self.collection.find_one({"_id": userID})
-            newFirstName = test_user["firstName"]
-            self.assertEqual(newFirstName, "test name")
-            self.collection.delete_one({"_id": userID})
-        except:
-            self.collection.delete_one({"_id": userID})
-            self.assertTrue(False)
+        response = self.session.post(self.base_url, {"newFirstName": "test name"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["data"]["user"]["firstName"], "test name")
+        test_user = self.collection.find_one({"email": self.TEST_USER_EMAIL})
+        newFirstName = test_user["firstName"]
+        self.assertEqual(newFirstName, "test name")
 
     def testChangeLastName(self):
-        try:
-            userID = bson.ObjectId(''.join(random.choices(string.digits, k = 24)))
-            self.collection.insert_one({"_id": userID, "firstName": "first name", "lastName": "last name"})
-            response = requests.post(self.base_url, {"userID": userID, "newLastName": "test name"})
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json()["data"]["newLastName"], "test name")
-            test_user = self.collection.find_one({"_id": userID})
-            newLastName = test_user["lastName"]
-            self.assertEqual(newLastName, "test name")
-            self.collection.delete_one({"_id": userID})
-        except:
-            self.collection.delete_one({"_id": userID})
-            self.assertTrue(False)
+        response = self.session.post(self.base_url, {"newLastName": "test name"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["data"]["user"]["lastName"], "test name")
+        test_user = self.collection.find_one({"email": self.TEST_USER_EMAIL})
+        newFirstName = test_user["firstName"]
+        self.assertEqual(newFirstName, "test name")
 
-    def testChangeBoth(self):
-        try:
-            userID = bson.ObjectId(''.join(random.choices(string.digits, k = 24)))
-            self.collection.insert_one({"_id": userID, "firstName": "first name", "lastName": "last name"})
-            response = requests.post(self.base_url, {"userID": userID, "newFirstName": "test name first", "newLastName": "test name last"})
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json()["data"]["newFirstName"], "test name first")
-            self.assertEqual(response.json()["data"]["newLastName"], "test name last")
-            test_user = self.collection.find_one({"_id": userID})
-            newFirstName = test_user["firstName"]
-            newLastName = test_user["lastName"]
-            self.assertEqual(newFirstName, "test name first")
-            self.assertEqual(newLastName, "test name last")
-            self.collection.delete_one({"_id": userID})
-        except:
-            self.collection.delete_one({"_id": userID})
-            self.assertTrue(False)
+    def testChangeBothNames(self):
+        response = self.session.post(self.base_url, {"newFirstName": "test name 1", "newLastName": "test name 2"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["data"]["user"]["firstName"], "test name 1")
+        self.assertEqual(response.json()["data"]["user"]["lastName"], "test name 2")
+        test_user = self.collection.find_one({"email": self.TEST_USER_EMAIL})
+        newFirstName = test_user["firstName"]
+        newLastName = test_user["lastName"]
+        self.assertEqual(newFirstName, "test name 1")
+        self.assertEqual(newLastName, "test name 2")
