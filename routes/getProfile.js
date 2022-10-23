41a8cd2f42de59ac2187b05d8c0211cfd3a987eb
Kevin Wu <kevin@kevinniuwu.com>
2022-10-23 05:06:30 -0400

    Add get profile route

---
+++ routes/getProfile.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", async function (req, res, next) {
+  if (req.user === undefined) {
+    return res.status(401).json({
+      status: "error",
+      data: null,
+      message: "Please log in first!",
+    });
+  }
+
+  return res.status(200).json({
+    status: "success",
+    data: {
+      firstName: req.user.firstName,
+      lastName: req.user.lastName,
+      email: req.user.email,
+      settings: req.user.settings.toJSON(),
+    },
+    message: null,
+  });
+});
+
+module.exports = router;
--- schemas/settings.js
+++ schemas/settings.js
@@ -1,23 +1,33 @@
 const mongoose = require("mongoose");
 
-const SettingsSchema = new mongoose.Schema({
-  trackHistory: {
-    type: Number,
-    required: true,
+const SettingsSchema = new mongoose.Schema(
+  {
+    trackHistory: {
+      type: Number,
+      required: true,
+    },
+    staySignedIn: {
+      type: Boolean,
+      required: true,
+    },
+    deepSearch: {
+      type: Boolean,
+      required: true,
+    },
+    darkMode: {
+      type: Boolean,
+      required: true,
+    },
   },
-  staySignedIn: {
-    type: Boolean,
-    required: true,
-  },
-  deepSearch: {
-    type: Boolean,
-    required: true,
-  },
-  darkMode: {
-    type: Boolean,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 const Settings = mongoose.model("Settings", SettingsSchema);
 
